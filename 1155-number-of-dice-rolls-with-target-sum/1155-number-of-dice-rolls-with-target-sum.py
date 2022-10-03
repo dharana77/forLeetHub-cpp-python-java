4dd5e9bd4839120bf3efd22ea15271af776dd308
dharana77 <77390758+dharana77@users.noreply.github.com>
2022-10-03 19:01:45 +0900

    Time: 1581 ms (17.12%), Space: 14.6 MB (70.35%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(2, n+1):
             for j in range(1, target+1):
                 t = 1
-                while t <= min(j,k):
+                while min((k-t), (j-t))>=0:
                     dp[i][j]+=dp[i-1][j-t]
                     t+=1
     
