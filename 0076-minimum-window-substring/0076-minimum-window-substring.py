fa0e8d03bbbdea6c516154d86950902ed37edce2
dharana77 <77390758+dharana77@users.noreply.github.com>
2022-10-22 23:50:02 +0900

    Time: 121 ms (86.36%), Space: 14.7 MB (83.90%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        need = collections.Counter(t)
+        missing = len(t)
+        left = start = end = 0
+
+        for right, char in enumerate(s, 1):
+            missing -= need[char] > 0
+            need[char] -= 1
+
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+
+                if not end or right - left <= end - start:
+                    start, end = left, right
+                    need[s[left]] += 1
+                    missing += 1
+                    left += 1
+        return s[start:end]
+
+
+    def minWindowCounter(self, s: str, t: str) -> str:
+        t_count = collections.Counter(t)
+        current_count = collections.Counter()
+
+        start = float('-inf')
+        end = float('-inf')
+
+        left = 0
+        for right, char in enumerate(s, 1):
+            current_count[char] += 1
+
+            while current_count & t_count == t_count:
+                if right - left < end - start:
+                    start, end = left, right
+                current_count[s[left]] -= 1
+                left += 1
+
+        return s[start:end] if end - start <= len(s) else '' 
+       
+        
