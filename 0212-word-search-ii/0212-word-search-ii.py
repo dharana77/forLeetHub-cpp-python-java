0a91a3d6c4e98a4c66fe9b64047088a287085e03
dharana77 <77390758+dharana77@users.noreply.github.com>
2022-11-06 19:27:45 +0900

    Time: 2858 ms (55.98%), Space: 16.4 MB (48.97%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.py
+++ 0212-word-search-ii/0212-word-search-ii.py
@@ -1,12 +1,3 @@
-## Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from collections import deque
-
-
 class Node(object):
     def __init__(self):
         self.endFlag = False
@@ -71,7 +62,6 @@ class Solution:
     dy = list()
     m = 0
     n = 0
-    board = list()
     words = list()
     ct = 0
 
@@ -82,14 +72,12 @@ class Solution:
         self.ct = 0
         self.m = 0
         self.n = 0
-        self.board = list()
         self.words = list()
         
     def findWords(self, board: list[list[str]], words: list[str]) -> list[str]:
         
         self.m = len(board)
         self.n = len(board[0])
-        self.board = board
         self.words = words
         
         self.trie = {}
@@ -100,10 +88,11 @@ class Solution:
                 if c not in t:
                     t[c] = {}
                 t = t[c]
-            t['#'] = '#'
 
+            t['#'] = '#'
+        
+        print(self.trie)
         self.answer = list()
-        # m, n = len(board), len(board[0])
         
         for y in range(self.m):
             for x in range(self.n):
@@ -111,57 +100,26 @@ class Solution:
 
         return self.answer
 
-        # for i, row in enumerate(board):
-        #     visited = [[False] * self.n for i in range(self.m)]
-        #     print(visited)
-        #     for j, col in  enumerate(row):
-        #         print(i,j)
-        #         self.bfs(i,j,"", visited)
-                
-        # return self.answer
     
-    
-    def bfs(self, ci, cj, cv, visited):
-        self.ct += 1
-        cv += self.board[ci][cj]
-        # print(ci, cj, cv)
-        visited[ci][cj] = True
-        temp = cv[::-1]
-        # print(visited)
-        if cv in self.words:
-            self.answer.append(cv)
-        if temp in self.words:
-            self.answer.append(temp)
-
-        for i in range(4):
-            nx = ci + self.dx[i]
-            ny = cj + self.dy[i]
-            # print("nxny", nx,ny, self.m, self.n)
-            if nx >= 0 and ny >=0 and nx < self.m and ny < self.n:
-                # print(visited[nx][ny])
-                if not visited[nx][ny]:
-                    visited[nx][ny] = True
-                    self.bfs(nx, ny, cv, visited)
-                    # visited[nx][ny] = False
-
     def dfs(self, board, node, y, x, word):
         if '#' in node:
             del node['#']
             self.answer.append(word)
-
+        
         if (y < 0) or y >= len(board) or x < 0 or x >= len(board[0]):
             return
-
+        
         char = board[y][x]
         if char not in node:
             return
-
+        
         next_node = node[char]
         board[y][x] = '@'
         for i in range(4):
             ny, nx = y + self.dy[i], x + self.dx[i]
             self.dfs(board, next_node, ny, nx, word+char)
         board[y][x] = char
-
+        
         if not next_node:
             del node[char]
+        
