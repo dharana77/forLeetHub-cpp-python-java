ecec942f56749aa93b4f2654c5d949778e3fb9b1
dharana77 <77390758+dharana77@users.noreply.github.com>
2022-11-17 15:36:45 +0900

    Time: 4 ms (81.38%), Space: 43.3 MB (29.83%) - LeetHub

--- 0223-rectangle-area/0223-rectangle-area.java
+++ 0223-rectangle-area/0223-rectangle-area.java
@@ -5,21 +5,12 @@ class Solution {
         int smallerRightX = Math.min(ax2, bx2);
         int smallerRightY = Math.min(ay2, by2);
         
-        // System.out.printf("%d%d%d%d", biggerLeftX, biggerLeftY, smallerRightX, smallerRightY);
         int same = (smallerRightX - biggerLeftX) * (smallerRightY - biggerLeftY);
         
         int sq1 = (ax2-ax1) * (ay2-ay1);
         int sq2 = (bx2-bx1) * (by2-by1);
         
-        System.out.println(sq1);
-        System.out.println(sq2);
-        System.out.println(same);
-        
         int answer = 0;
-        boolean case1 = ax1 >=bx1 && ay1 >= by1 && ax1 <= bx2 && ay1 <= by2;
-        boolean case2 = ax2 >=bx1 && ay1 >= by1 && ax2 <= bx2 && ay2 <= by2;
-        boolean case3 = bx1 >= ax1 && by1 >= ay1 && bx1 <= ax2 && by1 <= ay2;
-        boolean case4 = bx2 >= ax1 && by2 >= ay1 && bx2 <= ax2 && by2 <= ay2;
         boolean case5 = bx1 <= ax2 && bx2 >= ax1 && by1 <= ay2 && by2 >= ay1;
         
         if (case5){
