bdb8ae882e888cd3ced6097b051577461bc90595
dharana77 <77390758+dharana77@users.noreply.github.com>
2022-11-22 12:23:29 +0900

    Time: 6097 ms (16.30%), Space: 14.3 MB (44.73%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        
+        dp = [1e3] * (n + 1)
+        for i in range(1, n+1):
+            if sqrt(i) == int(sqrt(i)):
+                dp[i] = 1
+        
+        for i in range(1, n+1):
+            temp = list()
+            tar = int(sqrt(i)) + 1
+            print(i, tar)
+            for j in range(1, tar):
+                temp.append(j*j)
+
+            for j in temp:
+                dp[i] = min(dp[i], dp[i-j] + dp[j])
+        # print(dp)
+        return dp[n]
