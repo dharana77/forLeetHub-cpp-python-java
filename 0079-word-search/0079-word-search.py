2dbcf100b6cd8f907a0ca7e2b6be45a04aaaa9dd
dharana77 <77390758+dharana77@users.noreply.github.com>
2022-11-24 23:12:26 +0900

    Time: 122 ms (94.72%), Space: 14.1 MB (13.61%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,15 +1,21 @@
 from collections import deque
 
 class Solution:
+    ans = False
+    
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # m x n grid of cha
+        n , m = len(board), len(board[0])
         
-        directions = [[0,1],[1,0],[0,-1],[-1,0]]
-        m,n = len(board),len(board[0])
         
-        # (3) Inverse word if it's better
-        # If we find the right duplicates are less than the left duplicates, reverse the word.
+        dx = [1,-1,0,0]
+        dy = [0,0,-1,1]
+        visited = [[False] * m for _ in range(n)]
+        
+        
         left,right = 1,len(word)-2
         leftDup = rightDup = 1
+        
         while left<right:
             if word[left]==word[left-1]:
                 leftDup += 1
@@ -27,18 +33,36 @@ class Solution:
             left += 1
             right -= 1
             
-        def backtrack(i,j,k,visited):
-            if board[i][j] == word[k]:
-                if k==len(word)-1:
-                    return True
-                for xn,yn in directions:
-                    x,y = i+xn,j+yn
-                    if 0<=x<m and 0<=y<n and (x,y) not in visited:
-                        if backtrack(x,y,k+1,visited.union({(x,y)}))==True:
-                            return True
-            return False
+            
+        def dfs(x, y, s, visited, k):
+            # print(s)
+            if s[k] != word[k]:
+                return
+            
+            if s == word:
+                # print(s)
+                self.ans = True
+                return
+            
+            if len(s) > len(word):
+                return
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                # print(nx,ny)
+                if not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    dfs(nx,ny, s+board[nx][ny], visited, k+1)
+                    visited[nx][ny] = False
         
-        for i, j in product(range(m), range(n)):
-            if backtrack(i,j,0,{(i,j)}):
-                return True
-        return False
+        for i in range(n):
+            for j in range(m):
+                visited[i][j] = True
+                dfs(i,j, board[i][j], visited, 0)
+                visited[i][j] = False
+                
+        return self.ans
